fab004b454cb3651b2fdeba5f99c7ebd2c516446
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-07-24 09:10:09 +0530

    Play List Service spring boot project Iniitialized

---
+++ Backend/Play_List_Service/src/main/java/com/AudioHub/Play_List_Service/PlayListServiceApplication.java
@@ -0,0 +1,13 @@
+package com.AudioHub.Play_List_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayListServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayListServiceApplication.class, args);
+	}
+
+}
---
+++ Backend/Play_List_Service/src/test/java/com/AudioHub/Play_List_Service/PlayListServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AudioHub.Play_List_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayListServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
