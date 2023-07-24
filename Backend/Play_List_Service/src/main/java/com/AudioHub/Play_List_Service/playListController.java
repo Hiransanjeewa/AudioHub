c501eb7849bc6a6f71daca6c99bf797eb75359d3
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-07-24 13:48:07 +0530

    Endpoint type chaned to Get

--- Backend/Play_List_Service/src/main/java/com/AudioHub/Play_List_Service/playListController.java
+++ Backend/Play_List_Service/src/main/java/com/AudioHub/Play_List_Service/playListController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/playlist/")
 public class playListController {
 
-    @PostMapping(path = "getlist")
-    public String RemoveItemfromCart(@RequestParam Integer userid){
+    @GetMapping(path = "getlist")
+    public String GetPlayLists(@RequestParam Integer userid){
         return "Hello user this is your Playlist"+ userid;
     }
 
