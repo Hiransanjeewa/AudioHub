e5e4f5512a36d01fda62aa6aa04a644869016ea4
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-07-25 22:57:40 +0530

    Play list controller edited

--- Backend/Play_List_Service/src/main/java/com/AudioHub/Play_List_Service/playListController.java
+++ Backend/Play_List_Service/src/main/java/com/AudioHub/Play_List_Service/playListController.java
@@ -10,7 +10,7 @@ public class playListController {
 
     @GetMapping(path = "getlist")
     public String GetPlayLists(@RequestParam Integer userid){
-        return "Hello user this is your Playlist"+ userid;
+        return "Hello user this is your Playlist welcome"+ userid;
     }
 
 }
